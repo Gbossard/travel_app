50c68dfef7ba571dc5f563ad9b43387916535284
Gaëlle Bossard <bossard.gaelle@gmail.com>
2022-02-26 18:13:45 +0100

    Add color in theme

---
+++ lib/theme.dart
@@ -0,0 +1,12 @@
+///
+/// theme.dart
+///
+import 'package:flutter/material.dart';
+
+const greyLighter = Color(0xFFFCFFFD);
+const greyLight = Color(0xFFEAF4F4);
+const grey = Color(0xFF9EA0A9);
+
+const cyanLight = Color(0xFFCAF0F8);
+const middleBlue = Color(0xFF90E0EF);
+const pacificBlue = Color(0xFF00B4D8);
