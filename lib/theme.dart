10eb8ebee1fd221351254f42d3d2a87022eb243e
Gaëlle Bossard <bossard.gaelle@gmail.com>
2022-03-05 15:24:23 +0100

    Update theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -6,12 +6,14 @@ import 'package:flutter/material.dart';
 const greyLighter = Color(0xFFFCFFFD);
 const greyLight = Color(0xFFEAF4F4);
 const grey = Color(0xFF9EA0A9);
+const greyDark = Color(0xFF424242);
 
 const cyanLight = Color(0xFFCAF0F8);
 const middleBlue = Color(0xFF90E0EF);
 const pacificBlue = Color(0xFF00B4D8);
 const startCommandBlue = Color(0xFF0077B6);
 
+// DRAWER NAVIGATION
 var titleNavigationFirst = const TextStyle(
   fontFamily: "Montserrat",
   fontSize: 25,
@@ -21,5 +23,20 @@ var titleNavigationFirst = const TextStyle(
 var titleNavigationSecond = const TextStyle(
   fontFamily: "Montserrat",
   fontSize: 25,
-  color: pacificBlue
+  color: pacificBlue,
+);
+
+// TITLE HOME
+var titleHomeGrey = const TextStyle(
+  fontFamily: "Montserrat",
+  fontSize: 30,
+  color: grey,
+  fontWeight: FontWeight.w400
+);
+
+var titleHomeBlue = const TextStyle(
+  fontFamily: "Montserrat",
+  fontSize: 40,
+  color: pacificBlue,
+  fontWeight: FontWeight.bold
 );
