8e11a6d72d01d959bc81884cd7743cd9fe82b7e2
Gaëlle Bossard <bossard.gaelle@gmail.com>
2022-03-05 15:23:16 +0100

    Update titleHome widget

--- lib/widget/home/titleHome.dart
+++ lib/widget/home/titleHome.dart
@@ -6,9 +6,22 @@ import 'package:travel_app/theme.dart';
 
 Widget titleHome() {
   return Padding(
-    padding: EdgeInsets.only(top: 48, left: 28.8),
+    padding: EdgeInsets.only(top: 50, left: 15),
     child: Column(
-      
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Text('Go to ', style: titleHomeGrey), 
+            Text('Liberty,',style: titleHomeBlue)
+          ],
+        ),
+        Row(
+          children: [
+            Text('Go ', style: titleHomeGrey),
+            Text('Travel', style: titleHomeBlue)
+          ],
+        )
+      ],
     ),
   );
 }
