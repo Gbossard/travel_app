30997a00be814051262a66909ddc8978acb3d763
Gaëlle Bossard <bossard.gaelle@gmail.com>
2022-03-16 11:38:18 +0100

    Update unused import

--- lib/screen/account.dart
+++ lib/screen/account.dart
@@ -2,7 +2,6 @@
 /// screen/account.dart
 ///
 import 'package:flutter/material.dart';
-import 'package:travel_app/model/airtable_data_destination.dart';
 import 'package:travel_app/widget/account/profil_account.dart';
 import 'package:travel_app/widget/home/header_home.dart';
 
@@ -16,7 +15,6 @@ class AccountScreen extends StatefulWidget {
 class _AccountScreenState extends State<AccountScreen> {
   @override
   Widget build(BuildContext context) {
-    final AirtableData airtableData = AirtableData();
     
     return Container(
       child: ListView(
